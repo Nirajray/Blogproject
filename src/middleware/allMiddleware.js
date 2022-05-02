cd58f5c2ba27c53b4a2068dda7f3850030cd2610
Nirajray <nirajray5555@gmail.com>
2022-05-02 13:16:02 +0530

    my code

--- src/middleware/allMiddleware.js
+++ src/middleware/allMiddleware.js
@@ -26,27 +26,27 @@ const authorisation = async function (req, res, next) {
             return res.status(401).send({ status: false, msg: "token is invalid" });
         if (req.params.blogId) {
             let blog_id = req.params.blogId
-            if(!blog_id)  return res.status(400).send({ status: false, msg: "please provide id of blog" })
+            if (!blog_id) return res.status(400).send({ status: false, msg: "please provide id of blog" })
             if (!isValidObjectId(blog_id)) return res.status(400).send({ status: false, msg: "please provide a valid object id of blog" })
             let model = await blogModel.findById(blog_id)
             let authorId = model.author_id
             let authorLoggedIn = decodedToken.authorId
             if (authorId != authorLoggedIn) return res.status(403).send({ status: false, msg: 'author logged is not allowed to modify the requested users data' })
-        } 
+        }
         else {
             let value = req.query
-            let b={isDeleted:false}
-            let update = await blogModel.find(value,b);
+            let b = { isDeleted: false }
+            let update = await blogModel.find(value, b);
             if (update.length == 0) {
                 return res.status(404).send({ status: false, msg: "blog not found" });
-            } 
-            for (let i = 0; i < update.length; i++){
+            }
+            for (let i = 0; i < update.length; i++) {
                 if (update[i].author_id != decodedToken.authorId) {
                     return res.status(403).send({ status: false, msg: 'author logged is not allowed to modify the requested authors data' })
 
                 }
             }
-        } 
+        }
         next()
 
     }
@@ -60,4 +60,4 @@ const authorisation = async function (req, res, next) {
 
 
 module.exports.authentication = authentication
-module.exports.authorisation= authorisation
+module.exports.authorisation = authorisation
